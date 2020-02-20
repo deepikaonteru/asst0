c2c8ec789101329c2e9e0b8fb8465b92d06ae60a
Deepika Onteru <do230@ilab2.cs.rutgers.edu>
2020-02-20 15:03:43 -0500

    breaking code into functional elements

--- asst0.c
+++ asst0.c
@@ -1,2 +1,19 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 //hello
+
+int main(int argc, char *argv[]){
+
+
+
+
+}
+
+
+//read from a file and extract tokens
+//code linked lists for sorting purposes
+//insertion sort
+//quicksort
+//integer and string comparator (cast void* input insidee functions)
+//modify sort code to accept comparators
